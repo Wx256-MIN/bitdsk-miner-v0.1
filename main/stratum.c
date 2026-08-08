146027d42d14ba3cd64c6b42f10ad5ed1d04304e
Wx256-MIN <sanikazi78@gmail.com>
2026-08-08 17:42:50 +0530

    Update stratum.c

--- main/stratum.c
+++ main/stratum.c
@@ -3,6 +3,7 @@
  * (lwip BSD sockets), newline-delimited JSON-RPC (cJSON).
  */
 
+#include <inttypes.h>
 #include <string.h>
 #include <stdio.h>
 #include "stratum.h"
@@ -283,18 +284,18 @@ bool stratum_submit_share(const char *job_id, const char *extranonce2_hex,
 {
     static uint32_t s_submit_id = 100;
     char nonce_hex[9];
-    snprintf(nonce_hex, sizeof(nonce_hex), "%08x", nonce);
+    snprintf(nonce_hex, sizeof(nonce_hex), "%08" PRIx32, nonce);
 
     char req[512];
     if (version_bits != 0) {
         char version_hex[9];
-        snprintf(version_hex, sizeof(version_hex), "%08x", version_bits);
+        snprintf(version_hex, sizeof(version_hex), "%08" PRIx32, version_bits);
         snprintf(req, sizeof(req),
-                 "{\"id\":%u,\"method\":\"mining.submit\",\"params\":[\"%s\",\"%s\",\"%s\",\"%s\",\"%s\",\"%s\"]}",
+                 "{\"id\":%" PRIu32 ",\"method\":\"mining.submit\",\"params\":..."
                  s_submit_id++, s_username, job_id, extranonce2_hex, ntime_hex, nonce_hex, version_hex);
     } else {
         snprintf(req, sizeof(req),
-                 "{\"id\":%u,\"method\":\"mining.submit\",\"params\":[\"%s\",\"%s\",\"%s\",\"%s\",\"%s\"]}",
+                 "{\"id\":%" PRIu32 ",\"method\":\"mining.submit\",\"params\":..."
                  s_submit_id++, s_username, job_id, extranonce2_hex, ntime_hex, nonce_hex);
     }
     return send_line(req);
