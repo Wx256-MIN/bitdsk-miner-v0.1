eb9d0a4d3b8f3638efc78c55e58aacc24a437022
Wx256-MIN <sanikazi78@gmail.com>
2026-08-08 17:54:02 +0530

    Update stratum.c

--- main/stratum.c
+++ main/stratum.c
@@ -291,8 +291,12 @@ bool stratum_submit_share(const char *job_id, const char *extranonce2_hex,
         char version_hex[9];
         snprintf(version_hex, sizeof(version_hex), "%08" PRIx32, version_bits);
         snprintf(req, sizeof(req),
+         "{\"id\":%" PRIu32 ",\"method\":\"mining.submit\",\"params\":[\"%s\",\"%s\",\"%s\",\"%s\",\"%s\",\"%s\"]}",
+         s_submit_id++, s_username, job_id, extranonce2_hex, ntime_hex, nonce_hex, version_hex);
                  "{\"id\":%" PRIu32 ",\"method\":\"mining.submit\",\"params\":..."
-                 s_submit_id++, s_username, job_id, extranonce2_hex, ntime_hex, nonce_hex, version_hex);
+                 snprintf(req, sizeof(req),
+         "{\"id\":%" PRIu32 ",\"method\":\"mining.submit\",\"params\":[\"%s\",\"%s\",\"%s\",\"%s\",\"%s\"]}",
+         s_submit_id++, s_username, job_id, extranonce2_hex, ntime_hex, nonce_hex);
     } else {
         snprintf(req, sizeof(req),
                  "{\"id\":%" PRIu32 ",\"method\":\"mining.submit\",\"params\":..."
